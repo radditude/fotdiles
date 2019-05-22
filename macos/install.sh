acec53d00041a96540186a567b6824dcede7172c
CJ Horton <thecjhorton@gmail.com>
2019-05-22 02:13:38 -0500

    collecting dotfiles, round 1

---
+++ macos/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# finder should show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles YES
+# Specify iterm preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES_ROOT/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
