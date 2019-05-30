37194587bf4781c9df1dbe3c90d4b2aed93eb424
CJ <thecjhorton@gmail.com>
2019-05-29 22:05:32 -0500

    fix asdf install script

--- asdf/install.sh
+++ asdf/install.sh
@@ -2,7 +2,7 @@
 
 source $DOTFILES/functions/log
 
-if [ -d "$HOME/.asdf" ]
+if brew --prefix asdf
 then
   info "configuring asdf"
   asdf plugin-add ruby
@@ -10,8 +10,13 @@ then
   asdf plugin-add elixir
   asdf plugin-add erlang
   asdf plugin-update --all
-  cd $HOME && asdf install
+  # install keys for nodejs
+  bash $HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring
+  # run asdf install twice because sometimes language installs exit with 
+  # non-zero codes even if the install succeeds (looking at you, Erlang)
+  cd $HOME && asdf install && asdf install
 
+  echo ""
   success "asdf configuration complete!"
 else
   info "asdf not installed, skipping asdf configuration"
