3ad86eee54393d05c936b363f96b5684a756c6be
CJ Horton <thecjhorton@gmail.com>
2019-05-31 21:46:26 -0500

    n output

--- macos/install.sh
+++ macos/install.sh
@@ -3,6 +3,6 @@
 # finder should show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles YES
 # Specify iterm preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES_ROOT/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
