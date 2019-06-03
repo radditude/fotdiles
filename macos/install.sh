c38339b9203aff1cff3a827b4211d645e28d4f0e
CJ <thecjhorton@gmail.com>
2019-06-02 21:12:03 -0500

    correct shortcut to show all files

--- macos/install.sh
+++ macos/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # finder should show hidden files by default
-defaults write com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.Finder AppleShowAllFiles true
 # Specify iterm preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
