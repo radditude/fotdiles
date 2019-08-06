430fc1355ab67d4a1c1ecc790cae35c121746c02
CJ Horton <cjhorton@CJs-MacBook-Pro.local>
2019-08-05 23:45:02 -0500

    script install of zsh and oh-my-zsh

---
+++ zsh/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# make zsh the default shell if it isn't already
+if [[ "$SHELL" != *"zsh"* ]]; then
+	sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+fi
+
+# install oh-my-zsh if needed
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# update oh-my-zsh
+upgrade_oh_my_zsh
