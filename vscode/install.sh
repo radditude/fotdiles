9bd8183f0184c11fa519172916c8ee9d16454647
CJ <thecjhorton@gmail.com>
2019-08-09 20:55:09 -0500

    v1 vscode extension sync

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 
-setup() {
+source $DOTFILES/functions/log
+
+settings() {
 	ORIGINAL_SETTINGS="$HOME/Library/Application Support/Code/User/settings.json"
 	rm "$ORIGINAL_SETTINGS"
 	ln -s $DOTFILES/vscode/settings.json "$ORIGINAL_SETTINGS"
 }
 
-setup
+extensions() {
+	file="$DOTFILES/vscode/extensions"
+
+	for ex in $(cat $file); do
+		info "installing https://marketplace.visualstudio.com/items?itemName=$ex"
+		code --install-extension $ex
+	done
+
+	code --list-extensions >$file
+}
+
+main() {
+	info "syncing VS Code settings"
+	settings
+	info "installing VS Code extensions"
+	extensions
+	success "done with VS Code setup!"
+}
+
+main
