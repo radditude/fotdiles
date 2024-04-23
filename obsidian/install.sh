9c7e13a154f98202aeb096a1f43e25abc04109c8
CJ Horton <cjhorton@hashicorp.com>
2024-04-22 20:35:12 -0700

    latest

---
+++ obsidian/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash -e
+
+
+NOTES_DIR="$HOME/Documents/notes"
+VIM_CONFIG_SOURCE="$FOTDILES/obsidian/.obsidian.vimrc"
+
+for dir in $(ls -d $NOTES_DIR/*); do
+  ln -s $VIM_CONFIG_SOURCE $dir/.obsidian.vimrc
+done
