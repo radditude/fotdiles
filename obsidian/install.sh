f25e4dce4fc0c3e5720a4e96c5bb81f3b4f02e8b
CJ Horton <cjh@orton.dev>
2024-04-22 21:17:30 -0700

    bad alias

--- obsidian/install.sh
+++ obsidian/install.sh
@@ -2,7 +2,7 @@
 
 
 NOTES_DIR="$HOME/Documents/notes"
-VIM_CONFIG_SOURCE="$FOTDILES/obsidian/.obsidian.vimrc"
+VIM_CONFIG_SOURCE="$DOTFILES/obsidian/.obsidian.vimrc"
 
 for dir in $(ls -d $NOTES_DIR/*); do
   ln -s $VIM_CONFIG_SOURCE $dir/.obsidian.vimrc
